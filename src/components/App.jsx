ab47fbc48a275ea1e397051a85a7941a20c38675
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2023-04-20 16:02:38 +0300

    Searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Component } from "react"
+import {Searchbar} from "components/Searchbar/Searchbar"
+
+export class App extends Component {
+  render() {
+    return (
+     <Searchbar onSubmit={this.setSearchQuery} />
   );
+  }
+ 
 };
